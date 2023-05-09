8f65f3fd3997594bb4d01635439e4ea4cc5252a0
Lucas <genevasterm@protonmail.ch>
2023-05-09 13:31:24 -0500

    misc changes

--- Client/ClientMain.cs
+++ Client/ClientMain.cs
@@ -1,42 +1,42 @@
+using CitizenFX.Core;
+using CitizenFX.Core.UI;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using CitizenFX.Core;
-using CitizenFX.Core.UI;
 using static CitizenFX.Core.Native.API;
 
 namespace geneva_television.Client
 {
     public class ClientMain : BaseScript
     {
-        private readonly Dictionary<int, int> _televisionRooms = new()
+        protected readonly Dictionary<int, int> _televisionRooms = new()
         {
-            { 1411597561, GetHashKey("prop_tv_flat_01") },
-            { 1961042103, GetHashKey("des_tvsmash_start") },
-            { 1541752721, GetHashKey("prop_tv_flat_02") },
-            { -1073987335, GetHashKey("prop_tv_03") },
-            { -1913322317, GetHashKey("prop_trev_tv_01") }
+            { 1411597561, Game.GenerateHash("prop_tv_flat_01") },
+            { 1961042103, Game.GenerateHash("des_tvsmash_start") },
+            { 1541752721, Game.GenerateHash("prop_tv_flat_02") },
+            { -1073987335, Game.GenerateHash("prop_tv_03") },
+            { -1913322317, Game.GenerateHash("prop_trev_tv_01") }
         };
 
-        private readonly Dictionary<int, Vector3> _televisionPositions = new()
+        protected readonly Dictionary<int, Vector3> _televisionPositions = new()
         {
-            { 1411597561, new Vector3(2.5724f, 527.9989f, 176.1619f) },
-            { 1961042103, new Vector3(-808.3051f, 171.2623f, 77.2822f) },
-            { 1541752721, new Vector3(-1160.5024f, -1520.7598f, 10.7393f) },
-            { -1073987335, new Vector3(-9.8135f, -1440.9128f, 31.3654f) },
-            { -1913322317, new Vector3(1978.2303f, 3819.6504f, 34.2724f) }
+            { 1411597561, new(2.5724f, 527.9989f, 176.1619f) },
+            { 1961042103, new(-808.3051f, 171.2623f, 77.2822f) },
+            { 1541752721, new(-1160.5024f, -1520.7598f, 10.7393f) },
+            { -1073987335, new(-9.8135f, -1440.9128f, 31.3654f) },
+            { -1913322317, new(1978.2303f, 3819.6504f, 34.2724f) }
         };
 
-        private readonly Dictionary<int, Vector3> _televisionRotations = new()
+        protected readonly Dictionary<int, Vector3> _televisionRotations = new()
         {
-            { 1411597561, new Vector3(0f, 0f, -29.9488f) },
-            { 1961042103, new Vector3(1.8886f, 0f, 110.9232f) },
-            { 1541752721, new Vector3(0f, 0f, 60.061f) },
-            { -1073987335, new Vector3(0f, 0f, -134.3211f) },
-            { -1913322317, new Vector3(0f, 0f, -105.15f) }
+            { 1411597561, new(0f, 0f, -29.9488f) },
+            { 1961042103, new(1.8886f, 0f, 110.9232f) },
+            { 1541752721, new(0f, 0f, 60.061f) },
+            { -1073987335, new(0f, 0f, -134.3211f) },
+            { -1913322317, new(0f, 0f, -105.15f) }
         };
-        
-        private readonly Dictionary<int, string> _channels = new()
+
+        protected readonly Dictionary<int, string> _channels = new()
         {
             { 1, "PL_STD_CNT" },
             { 2, "PL_STD_WZL" },
@@ -57,18 +57,18 @@ namespace geneva_television.Client
             { 18, "PL_WEB_HOWITZER" },
             { 19, "PL_WEB_RANGERS" }
         };
-        
-        private Entity _closestTv;
-        private bool _tvOn;
-        private bool _focusingTv;
-        private int _renderTargetId;
-        private int _cam;
-        private int _currentChannel = 1;
-        private const float CurrentVolume = 0f;
-        private bool _isChangingVolume;
-        private const float VolumeChangeSpeed = 0.5f;
-        private const float VolumeChangeDelay = 0.2f;
-        private float _volumeChangeTimer;
+
+        protected Entity _closestTv;
+        protected bool _tvOn;
+        protected bool _focusingTv;
+        protected int _renderTargetId;
+        protected int _cam;
+        protected int _currentChannel = 1;
+        protected const float CurrentVolume = 0f;
+        protected bool _isChangingVolume;
+        protected const float VolumeChangeSpeed = 0.5f;
+        protected const float VolumeChangeDelay = 0.2f;
+        protected float _volumeChangeTimer;
 
         private async Task DrawTvTick()
         {
@@ -77,17 +77,15 @@ namespace geneva_television.Client
             SetScriptGfxDrawBehindPausemenu(true);
             DrawTvChannel(0.5f, 0.5f, 1f, 1f, 0f, 255, 255, 255, 255);
             SetTextRenderId(1);
-
-            await Task.FromResult(0);
         }
 
         private async Task HandleTvOnStuffTick()
         {
             Screen.DisplayHelpTextThisFrame("Use ~INPUT_MOVE_LEFT_ONLY~ ~INPUT_MOVE_RIGHT_ONLY~ to change the channel.~n~Use ~INPUT_MOVE_UP_ONLY~ ~INPUT_MOVE_DOWN_ONLY~ to change the volume.~n~Press ~INPUT_ENTER~ to turn off the TV.~n~Press ~INPUT_CONTEXT~ to stop watching.");
-            
+
             HideHudAndRadarThisFrame();
-            
-            if (Game.IsControlJustReleased(0, Control.Context))
+
+            if (Game.IsControlJustPressed(0, Control.Context))
             {
                 SetCamActive(_cam, false);
                 RenderScriptCams(false, false, 3000, true, false);
@@ -99,12 +97,15 @@ namespace geneva_television.Client
                 Tick -= HandleTvOnStuffTick;
             }
 
-            if (Game.IsControlJustReleased(0, Control.Enter))
+            if (Game.IsControlJustPressed(0, Control.Enter))
             {
-                int soundId = GetSoundId();
-                PlaySoundFrontend(soundId, "MICHAEL_SOFA_TV_ON_MASTER", null, false);
-                ReleaseSoundId(soundId);
-                
+                int soundId = Audio.PlaySoundFrontend("MICHAEL_SOFA_TV_ON_MASTER");
+                while (!Audio.HasSoundFinished(soundId))
+                {
+                    await Delay(0);
+                }
+                Audio.ReleaseSound(soundId);
+
                 Game.PlayerPed.IsPositionFrozen = false;
                 Game.PlayerPed.IsVisible = true;
                 Game.PlayerPed.IsInvincible = false;
@@ -112,10 +113,10 @@ namespace geneva_television.Client
                 RenderScriptCams(false, false, 3000, true, false);
                 SetPlayerControl(Game.Player.Handle, true, 0);
                 DestroyAllCams(false);
-                
+
                 SetTvChannel(-1);
                 EnableMovieSubtitles(false);
-                ReleaseAmbientAudioBank();
+                ReleaseScriptAudioBank();
                 _tvOn = false;
                 _focusingTv = false;
                 Tick -= DrawTvTick;
@@ -128,28 +129,43 @@ namespace geneva_television.Client
                 if (!_isChangingVolume)
                 {
                     _isChangingVolume = true;
-                    
-                    int soundId = GetSoundId();
-                    PlaySoundFrontend(soundId, "MICHAEL_SOFA_TV_CHANGE_CHANNEL_MASTER", null, false);
-                    ReleaseSoundId(soundId);
-                    
+
+                    int soundId = Audio.PlaySoundFrontend("MICHAEL_SOFA_TV_CHANGE_CHANNEL_MASTER");
+                    while (!Audio.HasSoundFinished(soundId))
+                    {
+                        await Delay(0);
+                    }
+                    Audio.ReleaseSound(soundId);
+
                     float currentVolume = GetTvVolume();
                     float newVolume = currentVolume + VolumeChangeSpeed;
-                    if (newVolume > 0f) newVolume = 0f;
+                    if (newVolume > 0f)
+                    {
+                        newVolume = 0f;
+                    }
+
                     SetTvVolume(newVolume);
                 }
                 else
                 {
                     _volumeChangeTimer += Game.LastFrameTime;
+
                     if (_volumeChangeTimer >= VolumeChangeDelay)
                     {
-                        int soundId = GetSoundId();
-                        PlaySoundFrontend(soundId, "MICHAEL_SOFA_TV_CHANGE_CHANNEL_MASTER", null, false);
-                        ReleaseSoundId(soundId);
-                        
+                        int soundId = Audio.PlaySoundFrontend("MICHAEL_SOFA_TV_CHANGE_CHANNEL_MASTER");
+                        while (!Audio.HasSoundFinished(soundId))
+                        {
+                            await Delay(0);
+                        }
+                        Audio.ReleaseSound(soundId);
+
                         float currentVolume = GetTvVolume();
                         float newVolume = currentVolume + VolumeChangeSpeed;
-                        if (newVolume > 0f) newVolume = 0f; 
+                        if (newVolume > 0f)
+                        {
+                            newVolume = 0f;
+                        }
+
                         SetTvVolume(newVolume);
                         _volumeChangeTimer = 0f;
                     }
@@ -161,14 +177,21 @@ namespace geneva_television.Client
                 if (!_isChangingVolume)
                 {
                     _isChangingVolume = true;
-                    
-                    int soundId = GetSoundId();
-                    PlaySoundFrontend(soundId, "MICHAEL_SOFA_TV_CHANGE_CHANNEL_MASTER", null, false);
-                    ReleaseSoundId(soundId);
-                    
+
+                    int soundId = Audio.PlaySoundFrontend("MICHAEL_SOFA_TV_CHANGE_CHANNEL_MASTER");
+                    while (!Audio.HasSoundFinished(soundId))
+                    {
+                        await Delay(0);
+                    }
+                    Audio.ReleaseSound(soundId);
+
                     float currentVolume = GetTvVolume();
                     float newVolume = currentVolume - VolumeChangeSpeed;
-                    if (newVolume < -36f) newVolume = -36f;
+                    if (newVolume < -36f)
+                    {
+                        newVolume = -36f;
+                    }
+
                     SetTvVolume(newVolume);
                 }
                 else
@@ -176,13 +199,20 @@ namespace geneva_television.Client
                     _volumeChangeTimer += Game.LastFrameTime;
                     if (_volumeChangeTimer >= VolumeChangeDelay)
                     {
-                        int soundId = GetSoundId();
-                        PlaySoundFrontend(soundId, "MICHAEL_SOFA_TV_CHANGE_CHANNEL_MASTER", null, false);
-                        ReleaseSoundId(soundId);
-                        
+                        int soundId = Audio.PlaySoundFrontend("MICHAEL_SOFA_TV_CHANGE_CHANNEL_MASTER");
+                        while (!Audio.HasSoundFinished(soundId))
+                        {
+                            await Delay(0);
+                        }
+                        Audio.ReleaseSound(soundId);
+
                         float currentVolume = GetTvVolume();
                         float newVolume = currentVolume - VolumeChangeSpeed;
-                        if (newVolume < -36f) newVolume = -36f;
+                        if (newVolume < -36f)
+                        {
+                            newVolume = -36f;
+                        }
+
                         SetTvVolume(newVolume);
                         _volumeChangeTimer = 0f;
                     }
@@ -195,7 +225,7 @@ namespace geneva_television.Client
                 _volumeChangeTimer = 0f;
             }
 
-            if (Game.IsControlJustReleased(0, Control.MoveLeftOnly))
+            if (Game.IsControlJustPressed(0, Control.MoveLeftOnly))
             {
                 _currentChannel -= 1;
 
@@ -211,19 +241,22 @@ namespace geneva_television.Client
                 {
                     _currentChannel = 4;
                 }
-                
-                int soundId = GetSoundId();
-                PlaySoundFrontend(soundId, "MICHAEL_SOFA_TV_CHANGE_CHANNEL_MASTER", null, false);
-                ReleaseSoundId(soundId);
+
+                int soundId = Audio.PlaySoundFrontend("MICHAEL_SOFA_TV_CHANGE_CHANNEL_MASTER");
+                while (!Audio.HasSoundFinished(soundId))
+                {
+                    await Delay(0);
+                }
+                Audio.ReleaseSound(soundId);
 
                 SetTvChannelPlaylist(_currentChannel, _channels[_currentChannel], false);
                 SetTvChannel(_currentChannel);
             }
-            
-            if (Game.IsControlJustReleased(0, Control.MoveRightOnly))
+
+            if (Game.IsControlJustPressed(0, Control.MoveRightOnly))
             {
                 _currentChannel += 1;
-                
+
                 if (_currentChannel < 1)
                 {
                     _currentChannel = _channels.Count;
@@ -236,16 +269,17 @@ namespace geneva_television.Client
                 {
                     _currentChannel = 6;
                 }
-                
-                int soundId = GetSoundId();
-                PlaySoundFrontend(soundId, "MICHAEL_SOFA_TV_CHANGE_CHANNEL_MASTER", null, false);
-                ReleaseSoundId(soundId);
+
+                int soundId = Audio.PlaySoundFrontend("MICHAEL_SOFA_TV_CHANGE_CHANNEL_MASTER");
+                while (!Audio.HasSoundFinished(soundId))
+                {
+                    await Delay(0);
+                }
+                Audio.ReleaseSound(soundId);
 
                 SetTvChannelPlaylist(_currentChannel, _channels[_currentChannel], false);
                 SetTvChannel(_currentChannel);
             }
-
-            await Task.FromResult(0);
         }
 
         private async Task TrackInteriorTick()
@@ -280,7 +314,7 @@ namespace geneva_television.Client
                 Game.PlayerPed.Task.LookAt(_closestTv, -1);
                 Game.PlayerPed.IsVisible = false;
                 Game.PlayerPed.IsInvincible = true;
-                
+
                 _focusingTv = true;
                 Tick += HandleTvOnStuffTick;
                 Tick += TrackInteriorTick;
@@ -288,12 +322,15 @@ namespace geneva_television.Client
 
             if (!_tvOn)
             {
-                while (!RequestAmbientAudioBank("SAFEHOUSE_MICHAEL_SIT_SOFA", false)) await Delay(0);
+                while (!RequestAmbientAudioBank("SAFEHOUSE_MICHAEL_SIT_SOFA", false))
+                {
+                    await Delay(0);
+                }
 
                 RegisterNamedRendertarget("tvscreen", false);
                 LinkNamedRendertarget(_closestTv.Model);
                 _renderTargetId = GetNamedRendertargetRenderId("tvscreen");
-                
+
                 SetTvChannel(-1);
                 SetTvChannelPlaylist(_currentChannel, _channels[_currentChannel], false);
                 SetTvVolume(CurrentVolume);
@@ -301,16 +338,19 @@ namespace geneva_television.Client
                 EnableMovieSubtitles(true);
                 SetTvAudioFrontend(false);
                 AttachTvAudioToEntity(_closestTv.Handle);
-                
+
                 Tick += DrawTvTick;
-                
-                int soundId = GetSoundId();
-                PlaySoundFrontend(soundId, "MICHAEL_SOFA_TV_ON_MASTER", null, false);
-                ReleaseSoundId(soundId);
-                
+
+                int soundId = Audio.PlaySoundFrontend("MICHAEL_SOFA_TV_ON_MASTER");
+                while (!Audio.HasSoundFinished(soundId))
+                {
+                    await Delay(0);
+                }
+                Audio.ReleaseSound(soundId);
+
                 _tvOn = true;
                 _focusingTv = true;
-                
+
                 Vector3 televisionPosition = _televisionPositions[GetRoomKeyFromEntity(_closestTv.Handle)];
                 Vector3 televisionRotation = _televisionRotations[GetRoomKeyFromEntity(_closestTv.Handle)];
                 _cam = CreateCamWithParams("DEFAULT_SCRIPTED_CAMERA", televisionPosition.X, televisionPosition.Y, televisionPosition.Z, televisionRotation.X, televisionRotation.Y, televisionRotation.Z, 50f, false, 2);
@@ -323,7 +363,7 @@ namespace geneva_television.Client
                 Game.PlayerPed.Task.LookAt(_closestTv, -1);
                 Game.PlayerPed.IsVisible = false;
                 Game.PlayerPed.IsInvincible = true;
-                
+
                 Tick += HandleTvOnStuffTick;
                 Tick += TrackInteriorTick;
             }
@@ -343,20 +383,20 @@ namespace geneva_television.Client
                 await Delay(3000);
                 return;
             }
-            
+
             _closestTv = entity;
-            
+
             if (Vector3.DistanceSquared(_closestTv.Position, plyPos) > 3f)
             {
                 await Delay(1500);
                 return;
             }
-                
+
             if (!_focusingTv)
             {
                 Screen.DisplayHelpTextThisFrame(_tvOn ? "Press ~INPUT_CONTEXT~ to watch TV." : "Press ~INPUT_CONTEXT~ to turn on the TV.");
-                
-                if (Game.IsControlJustReleased(0, Control.Context))
+
+                if (Game.IsControlJustPressed(0, Control.Context))
                 {
                     await WatchTv();
                 }
@@ -371,10 +411,10 @@ namespace geneva_television.Client
                 await Delay(1500);
                 return;
             }
-            
+
             int currentInterior = GetInteriorFromEntity(Game.PlayerPed.Handle);
             int tvInterior = GetInteriorFromEntity(_closestTv.Handle);
-            
+
             if (currentInterior == tvInterior && Game.PlayerPed.Weapons.Current.Hash != WeaponHash.Unarmed)
             {
                 Screen.DisplayHelpTextThisFrame("A weapon cannot be equipped when in a safehouse.");
@@ -388,5 +428,5 @@ namespace geneva_television.Client
 
             await Delay(2000);
         }
-    }    
+    }
 }
